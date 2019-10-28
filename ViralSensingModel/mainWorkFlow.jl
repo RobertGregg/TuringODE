a9d681e7f36d0da140172055cc45fa8f64430e9c
TheSilverhead <jordan.john.weaver@gmail.com>
2019-10-28 13:57:06 -0400

    god help me

--- ViralSensingModel/mainWorkFlow.jl
+++ ViralSensingModel/mainWorkFlow.jl
@@ -27,18 +27,15 @@ end
 ###############################################################
 #Some Model Information needed for MCMC (parameters and States)
 parNames = [:k12, :k13,:k14,
-            :k21,
-            :r31,:k31,
+            :k21, :tau2,
+            :k31,:k32,
             :k41,:k42,
-            :k51,:k52,
-            :k61,:tau6,
-            :k71,
-            :k81,:k82,
-            :k91,
-            :k10_1,:k10_2,
-            :k11_1,:k11_2]
+            :k51,
+            :k61,
+            :k71, :k72,
+            :k81,:k82]
 
-varNames = ["IFN","IFNenv","STAT1","STATP2n","IRF7","IRF7Pn","Target","Eclipse","Productive","Virus"]
+varNames = ["IFN","IFNe","STATP2n","IRF7","IRF7P","Infected","Virus"]
 #Set the number of parameters and states
 parNum = length(parNames)
 varNum = length(varNames)
@@ -47,12 +44,10 @@ modelInfo = ModelInformation(parNames,varNames)
 ## Define any constants for the model
 const k11 = 0.0
 const n=3
-const TJtot = 0.0001
-const tau1 = 0.3
-const tau2 = 0.3
-const tau3 = 0.7
-const tau4 = 1.2
-const tau5 = 0.7
+const JAKtot = 0.0001
+const tau3 = 1.238 #Cambridge 2011
+const tau4 = 0.347 #Sharova 2009
+const tau5 = 6.93 #Prakash 2006
 #Can be defined here or just as a number in the equations
 
 function Model!(dy,y,par,t)
@@ -61,65 +56,53 @@ function Model!(dy,y,par,t)
   k12=par[1]
   k13=par[2]
   k14=par[3]
-  #IFN_env, ODE 2 parameters
+  #IFNe, ODE 2 parameters
   k21=par[4]
-  #STAT, ODE 3 parameters
-  r31=par[5]
+  tau2=par[5]
+  #STATP, ODE 3 parameters
   k31=par[6]
-  #STATP, ODE 4 parameters
-  k41=par[7]
-  k42=par[8]
-  #IRF7, ODE 5 parameters
-  k51=par[9]
-  k52=par[10]
-  #IRF7P, ODE 6 parameters
+  k32=par[7]
+  #IRF7, ODE 4 parameters
+  k41=par[8]
+  k42=par[9]
+  #IRF7P, ODE 5 parameters
+  k51=par[10]
+  #Infected cells, ODE 6 parameters
   k61=par[11]
-  tau6=par[12]
-  #Target cells, ODE 7 paramters
-  k71=par[13]
-  #Eclipse infected cells, ODE 8 parameters
-  k81=par[14]
-  k82=par[15]
-  #Productive infected cells, ODE 9 parameters
-  k91=par[16]
-  #Viral count, ODE 10 parameters
-  k10_1=par[17]
-  k10_2=par[18]
+  #Viral replication, ODE 7 paramters
+  k71=par[12]
+  k72=par[13]
   #TJ Constants
   #TJ describes the binding of IFN and SOCS feedback
-  k11_1=par[19]
-  k11_2=par[20]
-  TJ=TJtot*(y[2]/(k11_1+y[2])*(1.0/(1.0+k11_2))) #Eq. 11
+  k81=par[14]
+  k82=par[15]
+  JAK=JAKtot*(y[2]/(k81+y[2])*(1.0/(1.0+k82))) #Eq. 11
 
   #ODE System
-  v=y[10]/(10^4) #scale virus effects
-  i1=y[8]/(10^4) #scale eclipse cell effects
-  dy[1]=(k11*v)+(k12*(v^n))/(k13+(v^n))+k14*y[6]-y[1]*tau1 #IFN in cytoplasm
-  dy[2]=(k21*y[1])-(y[2]*tau2) #IFN in environment
-  dy[3]=r31+k31*y[4]-y[3]*tau3 #STAT in cytoplasm
-  dy[4]=(k41*TJ*y[3])/(k42+y[3])-y[4]*tau4 #STATP
-  dy[5]=k51*y[4]+k52*y[6]-y[5]*tau5 #IRF7
-  dy[6]=k61*y[5]-y[6]*tau6 #IRF7P
-  dy[7]=-k71*y[7]*y[10] #Uninfected target cells
-  dy[8]=k71*y[7]*y[10]-(k81*i1)/(1+k82*y[2]) #Eclipse infected cells
-  dy[9]=(k81*i1)/(1+k82*y[2])-k91*y[9] #Productive infected cells
-  dy[10]=(k10_1*y[9]*v^n)/(y[2]*v^n)-k10_2*y[10] #Virus count
+  v=y[7]/(10^4) #scale virus effects
+  dy[1]=k11*v+(k12*(v^n))/(k13+(v^n))+k14*y[5]-k21*y[1] #IFN in cytoplasm
+  dy[2]=k21*y[1]-y[2]*tau2 #IFN in environment
+  dy[3]=(k31*JAK)/(k32+JAK)-y[3]*tau3 #STATP
+  dy[4]=k41*y[3]+k42*y[5]-y[4]*tau4 #IRF7
+  dy[5]=k51*y[4]-y[5]*tau5 #IRF7P
+  dy[6]=-k61*y[6] #Productive infected cells
+  dy[7]=(k71*y[6]*(v^n))/(y[2]*(v^n))-k72*y[7] #Virus count
 
 end
 
 #Define information for ODE model
 p=rand(parNum) #Parameter values
-u0 = [7.94, 0, 262.3, 12.2, 14.15, 0, 0, 250000, 0, 7.5E-2] #Initial Conditions
+u0 = [7.94, 0, 12.2, 14.15, 0, 250000, 7.5E-2] #Initial Conditions
 tspan = (0.25,24.0) #Time (start, end)
 
 #Contruct the ODE Problem
 prob = ODEProblem(Model!,u0,tspan,p)
 alg = Rodas5()  #ODE solver
 
-#This is just me testing the ODEs
+#Testing the ODEs
 sol = solve(prob,alg)
-#plot(sol,layout=varNum,legend=false, framestyle=:box,title=[v for i=1:1, v in varNames])
-#xlabel!("")
+plot(sol,layout=varNum,legend=false, framestyle=:box,title=[v for i=1:1, v in varNames])
+xlabel!("")
 
 ###############################################################
                     # 2. Import the data
@@ -144,7 +127,7 @@ dataTransform = ConvertData(data)
 ###############################################################
 
 #What states are being measured?
-const allMeasuredStates = [1,3,4,5,10] #IFN, STAT, STATp, IRF7, Virus
+const allMeasuredStates = [1,3,4,7] #IFN, STATp, IRF7, Virus
 
 #This function takes in the solved ODE, and outputs the observable species
 function ObserveTransform(sol,measuredTime,currentStates)
@@ -156,12 +139,12 @@ function ObserveTransform(sol,measuredTime,currentStates)
   #Create a vector of control data
   timeFilter = x->x==measuredTime
 
-if 10 ∉ measuredIdx #if not virus
+if 7 ∉ measuredIdx #if not virus
   #Get the control values and calculate a lfc
   controlVec = convert(Vector,control[findfirst(timeFilter,control.Time),3:end])
   obversedSpecies = @. log2(max(obversedSpecies,0.0)+1.0) - log2(controlVec)
 
-elseif 10 ∈ measuredIdx && length(measuredIdx)==1 #Only Virus measured
+elseif 7 ∈ measuredIdx && length(measuredIdx)==1 #Only Virus measured
   obversedSpecies = obversedSpecies
 else #Both virus and species are measured
   #Get the control values and calculate a lfc, but skip the virus
@@ -178,7 +161,7 @@ end
 
 #Provide any prior knowledge for the parameters
 #priors = fill(FlatPos(0.0),length(prob.p))
-priors = fill(Uniform(0.0,1000.0),length(prob.p))
+priors = fill(Uniform(0.0,100.0),length(prob.p))
 
 #How many MCMC sample do you want
 mcmcSamples = 1000
@@ -218,7 +201,7 @@ savefig(runAvePlot,"./Figures/RunAve.pdf")
 
 
 #Create a plot fitting "best" parameter values
-@df data plot(:Time,[:IFN,:STAT,:STATP,:IRF7,:Virus],markershape=:auto,
+@df data plot(:Time,[:IFN,:STATP,:IRF7,:Virus],markershape=:auto,
               layout=length(allMeasuredStates),legend=false)
 
 #Retrieve the chains and information about the chains
@@ -246,7 +229,7 @@ for t=1:length(newSol.t)
 end
 
 plot!(newSol.t,plotBestSol[:,allMeasuredStates],layout=length(allMeasuredStates),legend=false,title=[v for i=1:1, v in varNames[allMeasuredStates]])
-#plot!(newSol,vars=allMeasuredStates,layout=length(allMeasuredStates))
+plot!(newSol,vars=allMeasuredStates,layout=5)
 savefig("./Figures/DataFit.pdf")
 
 
