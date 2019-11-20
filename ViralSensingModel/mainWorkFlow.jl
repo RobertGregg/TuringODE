5545f5ddecca06b75953ff15efe2680605c4080f
TheSilverhead <jordan.john.weaver@gmail.com>
2019-11-20 10:08:34 -0500

    Troubleshooting

--- ViralSensingModel/mainWorkFlow.jl
+++ ViralSensingModel/mainWorkFlow.jl
@@ -29,14 +29,13 @@ end
 #Some Model Information needed for MCMC (parameters and States)
 parNames = [:k12, :k13,:k14,
             :k21, :tau2,
-            :k31,:k32,
+            :k31,:k32, :k33,
             :k41,:k42,
             :k51,
             :k61,
-            :k71, :k72,
-            :k81,:k82]
+            :k71, :k72]
 
-varNames = ["IFN","IFNe","STATP2n","IRF7","IRF7P","Infected","Virus"]
+varNames = ["IFN","IFNe","STATP","IRF7","IRF7P","Infected","Virus"]
 #Set the number of parameters and states
 parNum = length(parNames)
 varNum = length(varNames)
@@ -45,7 +44,6 @@ modelInfo = ModelInformation(parNames,varNames)
 ## Define any constants for the model
 const k11 = 0.0
 const n=3
-const JAKtot = 0.0001
 const tau3 = 1.238 #Cambridge 2011
 const tau4 = 0.347 #Sharova 2009
 const tau5 = 6.93 #Prakash 2006
@@ -63,27 +61,23 @@ function Model!(dy,y,par,t)
   #STATP, ODE 3 parameters
   k31=par[6]
   k32=par[7]
+  k33=par[8]
   #IRF7, ODE 4 parameters
-  k41=par[8]
-  k42=par[9]
+  k41=par[9]
+  k42=par[10]
   #IRF7P, ODE 5 parameters
-  k51=par[10]
+  k51=par[11]
   #Infected cells, ODE 6 parameters
-  k61=par[11]
+  k61=par[12]
   #Viral replication, ODE 7 paramters
-  k71=par[12]
-  k72=par[13]
-  #TJ Constants
-  #TJ describes the binding of IFN and SOCS feedback
-  k81=par[14]
-  k82=par[15]
-  JAK=JAKtot*(y[2]/(k81+y[2])*(1.0/(1.0+k82))) #Eq. 11
+  k71=par[13]
+  k72=par[14]
 
   #ODE System
   v=y[7]/(10^4) #scale virus effects
-  dy[1]=k11*v+(k12*(v^n))/(k13+(v^n))+k14*y[5]-k21*y[1] #IFN in cytoplasm
+  dy[1]=k11*v+(k12*(v^n))/(k13+(v^n))+k14*y[5]-k21*y[1] #IFN in cells
   dy[2]=k21*y[1]-y[2]*tau2 #IFN in environment
-  dy[3]=(k31*JAK)/(k32+JAK)-y[3]*tau3 #STATP
+  dy[3]=(k31*y[2])/(k32+k33*y[2])-y[3]*tau3 #STATP
   dy[4]=k41*y[3]+k42*y[5]-y[4]*tau4 #IRF7
   dy[5]=k51*y[4]-y[5]*tau5 #IRF7P
   dy[6]=-k61*y[6] #Productive infected cells
@@ -93,7 +87,7 @@ end
 
 #Define information for ODE model
 p=rand(parNum) #Parameter values
-u0 = [7.94, 0.01, 12.2, 14.15, 0.01, 250000, 7.5E-2] #Initial Conditions
+u0 = [7.94, 0.01, 12.2, 14.15, 0.01, 1, 7.5E-2] #Initial Conditions
 tspan = (0.25,24.0) #Time (start, end)
 
 #Contruct the ODE Problem
@@ -144,7 +138,6 @@ if 7 ∉ measuredIdx #if not virus
   #Get the control values and calculate a lfc
   controlVec = convert(Vector,control[findfirst(timeFilter,control.Time),3:end])
   obversedSpecies = @. log2(maximum(obversedSpecies,0.0)+1.0) - log2(controlVec)
-
 elseif 7 ∈ measuredIdx && length(measuredIdx)==1 #Only Virus measured
   obversedSpecies = obversedSpecies
 else #Both virus and species are measured
@@ -152,7 +145,6 @@ else #Both virus and species are measured
   controlVec = convert(Vector,control[findfirst(timeFilter,control.Time),3:end])
   obversedSpecies[1:end-1] = @. log2(maximum(obversedSpecies[1:end-1],0.0)+1.0) - log2(controlVec)
 end
-
   return obversedSpecies
 end
 
@@ -163,11 +155,11 @@ end
 #Provide any prior knowledge for the parameters
 #priors = fill(FlatPos(0.0),length(prob.p))
 priors = fill(Uniform(0.0,100.0),length(prob.p))
-priors[5]=Uniform(0,10) #tau2, IFNe degradation
-priors[11]=Uniform(0,1) #k61 Infected cell clearance
+priors[5]=Uniform(0,1) #tau2, IFNe degradation
+priors[11]=Uniform(0,1) #k61, Infected cell clearance
 priors[13]=Uniform(0,1) #k72, viral clearance
 #How many MCMC sample do you want
-mcmcSamples = 10
+mcmcSamples = 1000
 
 #Gather all the information to one structure
 sampleProblem = MCMCSetup(modelInfo,prob,alg,dataTransform,mcmcSamples,priors)
@@ -213,7 +205,8 @@ bestPar = BestParSet(result,parNames) #ignore the std
 
 
 #Rerun the problem with these parameters
-newProb = remake(prob, p=bestPar)
+#newProb = remake(prob, p=bestPar)
+newProb = ODEProblem(Model!,u0,[0.25,18],bestPar)
 newSol = solve(newProb,alg)
 
 plotBestSol = zeros(length(newSol.t),varNum)
